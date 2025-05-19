addc028ae2c3d6e9d028ec6e3fcb16208802478c
XxMattxX5 <116413060+XxMattxX5@users.noreply.github.com>
2025-05-19 13:04:30 -0400

    Fixed status bar not showing up green

--- passwordmanager/app/src/main/java/com/example/password_manager/ui/BaseActivity.kt
+++ passwordmanager/app/src/main/java/com/example/password_manager/ui/BaseActivity.kt
@@ -1,14 +1,20 @@
 package com.example.password_manager.ui
 
-
 import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.lifecycleScope
 import com.example.password_manager.R
 import com.example.password_manager.utils.AuthManager
 import kotlinx.coroutines.launch
+
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -30,6 +36,33 @@ abstract class BaseActivity : AppCompatActivity() {
         lifecycleScope.launch {
             setLogoutVisibility()
         }
+
+        val topBar = findViewById<LinearLayout>(R.id.topBar)
+        val originalTopPadding = topBar.paddingTop
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        ViewCompat.setOnApplyWindowInsetsListener(fullView) { view, insets ->
+
+            val systemInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val imeInsets = insets.getInsets(WindowInsetsCompat.Type.ime())
+
+            // Add top padding for status bar
+            topBar.updatePadding(top = originalTopPadding + systemInsets.top)
+
+            // Add bottom padding only if keyboard is visible
+            val bottomPadding = if (insets.isVisible(WindowInsetsCompat.Type.ime())) imeInsets.bottom else systemInsets.bottom
+
+            view.updatePadding(
+                bottom = bottomPadding,
+                left = systemInsets.left,
+                right = systemInsets.right
+            )
+
+            // Return insets so system can still do default behavior
+            insets
+        }
+        
     }
 
     // Ensures login status is stored in the isLogged variable before updating top bar
